6808e3a1ba67ed4f814bbed0e8ff71d8c45d990c
Sebastian Diaz Monfort <diazmonfortsebastian@gmail.com>
2024-02-22 21:36:48 -0300

    cambios

--- processListener.js
+++
@@ -1,8 +0,0 @@
-process.on('exit', code => {
-    console.log('Evento que se ejecuta antes de salir del proceso', code)
-} )
-process.on('uncaughtException', exception => {
-    console.log('Evento que ejecuta excepciones', exception)
-})
-
-console.log('Ejecutando sentencia!')
